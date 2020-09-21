894ccd8b79fafa2ed2cb0824e2581d463052355e
oac89 <oacastro2@gmail.com>
2020-09-21 07:24:23 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
-# Make your shoe class here!
+require 'pry'
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition 
+
+    def initialize(brand) 
+        @brand = brand 
+    end 
+
+    def cobbler
+        @condition = repaired 
+    end 
+
+    def cobbler(condition)
+        @condition = new 
+    end 
+    
+end 
